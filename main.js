0a1c1406a4d3f0ee4ff8af3e747185f8e53145f8
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-09-01 16:32:52 -0400

    running clock

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 
-setInterval(clockdisplay, 1000);
-function clockdisplay() {
+
+setInterval(setTime, 1000);
+
+
+function setTime() {
   var time = new Date();
   var hrs = time.getHours();
   var min = time.getMinutes();
@@ -13,6 +16,15 @@ function clockdisplay() {
   if (hrs == 0) {
     hrs = 12;
   }
+  if (hrs < 10) {
+    hrs = '0' + hrs;
+  }
+  if (min < 10) {
+    min = '0' + min;
+  }
+  if (sec < 10) {
+    sec = '0' + sec;
+  }
 
-  document.getElementById('clock-display');
-}
+  document.querySelector(".clock-display").innerHTML = hrs + ':' + min + ':' + sec;
+};
