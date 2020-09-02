ba9f5a83358eb6d5de506b18870b607e07d8c407
joshgoshbgosh <69131589+joshgoshbgosh@users.noreply.github.com>
2020-09-02 19:52:14 -0400

    hex hover

--- main.js
+++ main.js
@@ -1,34 +1,35 @@
 
 //running clock//
 
-setInterval(setTime, 1000);
+//setInterval(setTime, 1000);
 
 
-function setTime() {
+(function setTime() {
+
   var time = new Date();
-  var hrs = time.getHours();
-  var min = time.getMinutes();
-  var sec = time.getSeconds();
+  var hrs;
+  var min;
+  var sec;
 
-  if (hrs > 12) {
-    hrs = hrs - 12;
-  }
+  //if (hrs > 12) {
+  //  hrs = hrs - 12;
+  //}
 
-  if (hrs == 0) {
-    hrs = 12;
-  }
-  if (hrs < 10) {
-    hrs = '0' + hrs;
-  }
-  if (min < 10) {
-    min = '0' + min;
-  }
-  if (sec < 10) {
-    sec = '0' + sec;
-  }
+  //if (hrs == 0) {
+  //  hrs = 12;
+  //}
+  //if (hrs < 10) {
+  //  hrs = '0' + hrs;
+  //}
+  //if (min < 10) {
+  //  min = '0' + min;
+  //}
+  //if (sec < 10) {
+  //  sec = '0' + sec;
+  //}
 
   document.querySelector(".clock-display").innerHTML = hrs + ':' + min + ':' + sec;
-
+  document.querySelector(".clock").style.background=`#${hrs}${min}${sec}`;
 
 //console.loging 1 minute//
 console.log(sec / 60);
@@ -41,16 +42,38 @@ $progressBar.style.width = width;
 
 //hexidecimal color//
 
-var doc = document.getElementById("background");
-var color = ["black", "blue", "brown", "green"];
-var i = 0;
-function change() {
-  doc.style.backgroundColor = color[i];
-  i++;
+//var doc = document.getElementById("background");
+//var color = ["black", "blue", "brown", "green"];
+//var i = 0;
+//function change() {
+//  doc.style.backgroundColor = color[i];
+//  i++;
 
-  if(i > color.length - 1) {
-    i = 0;
-  }
-}
-setInterval(change, 1000);
-}
+//  if(i > color.length - 1) {
+//    i = 0;
+//  }
+//}
+var colorClock = document.querySelector(".clock");
+var clockDisplay=document.querySelector(".clock-display");
+var isHovering=false;
+clockDisplay.addEventListener("mouseover", function() {
+            isHovering = true;
+        });
+        clockDisplay.addEventListener("mouseout" , function() {
+            isHovering = false;
+        });
+setInterval(function() {
+            currentTime = new Date()
+            if(isHovering) {
+                hrs = ("0" + currentTime.getHours().toString(16)).slice(-2);
+                min = ("0" + currentTime.getMinutes().toString(16)).slice(-2);
+                sec = ("0" + currentTime.getSeconds().toString(16)).slice(-2);
+            } else {
+                hrs = ("0" + currentTime.getHours()).slice(-2);
+                min = ("0" + currentTime.getMinutes()).slice(-2);
+                sec = ("0" + currentTime.getSeconds()).slice(-2);
+            }
+            clockDisplay.textContent = hrs + ":" + min + ":" + sec;
+            colorClock.style.backgroundColor = "#" + hrs.toString(16) + min.toString(16) + sec.toString(16);
+        }, 1000);
+})();
